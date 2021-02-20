38b3040c6adff652b7e043e94b7428e66c6e4531
Nakzoo <bayram_0310@hotmail.com>
2021-02-20 05:07:33 +0300

    Add project files.

---
+++ MyDictionaryy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionaryy<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionaryy()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            
+            TKey[] tempArrayKey = keys;
+            TValue[] tempArrayValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i <tempArrayKey.Length; i++) 
+            {
+                keys[i] = tempArrayKey[i];
+            }
+
+            for (int i = 0; i < tempArrayValue.Length; i++)
+            {
+                values[i] = tempArrayValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            Console.WriteLine("Ogrenci:{0}", key);
+            Console.WriteLine("No :{0}", value);
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> StudentandNo = new Dictionary<string, int>();
+            StudentandNo.Add("eren oktay",26);
+            StudentandNo.Add("Engin Demirog", 35);
+
+            //Öğelere ulaşmak için - 1
+            //foreach (var item in StudentandNo)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // Öğelere ulaşmak için -2
+            //var items = StudentandNo.OrderBy(i => i.Key);
+            //foreach (var item in items)
+            //{
+            //    Console.WriteLine("Adı >{0} No >{1}", item.Key, item.Value);
+            //}
+
+
+
+
+
+            MyDictionaryy<string, int> myDictionaryy = new MyDictionaryy<string, int>();
+            myDictionaryy.Add("Engin Demirog", 45);
+            myDictionaryy.Add("Bayram Ozkan", 38);
+
+           
+           
+
+
+
+
+        }
+    }
+}
